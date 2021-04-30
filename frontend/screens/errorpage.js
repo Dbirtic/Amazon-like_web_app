8c8380d23fbd71a687117a41c06f489e050fedf6
Dbirtic <Dominik.birtic666@gmail.com>
2021-04-30 19:20:33 +0200

    added few extensions to the VScode and installed eslint extension to be prettier

--- frontend/screens/errorpage.js
+++ frontend/screens/errorpage.js
@@ -1,10 +1,8 @@
 const errorPage = {
-    render: () => {
-        return `
+    render: () => `
             <div>404 Error</div>
             <p>Sorry, page not found...</p>
-            `;
-    },
+            `,
 };
 
 export default errorPage;
